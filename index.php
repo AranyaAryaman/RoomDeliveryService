4b60bebac72743df1b8521fc08c39f539b070f92
AranyaAryaman <aryamanaranya@gmail.com>
2019-05-01 20:29:26 +0530

    Main Page Added

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
+<?php
+  if(isset($_POST["logstud"])){
+    header('location: login.php');
+  }
+
+  if(isset($_POST["loghot"])){
+    header('location: hotel_login.php');
+  }
+
+?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-<title> Login Page </title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
+<title> First Page </title>
 <body>
-    <div class= "loginbox">
-      <img src="avatar.png" class="avatar">
-      <h1>
-        Login Here
-      </h1>
-      <form action="Database.php" method="POST" >
+      <form action="index.php" method="POST" >
         <div class="form_input">
-        <p>Username</p>
-        <input type="text" name="username" placeholder="Enter Your Username">
+        <input type="submit" name="logstud" value="Students">
         </div>
-        <div class="form_input">
-        <p>Password</p>
-        <input type="password" name="password" placeholder="Enter Your Password">
-        </div>
-        <input type="submit" name="submit" value="Login">
-        <a href="class/forgot.php">Forgot password?</a><br>
-        <a href="class/signup.php">Create a new account</a>
       </form>
-    </div>
+
+      <form action="index.php" method="POST" >
+      <div class="form_input">
+      <input type="submit" name="loghot" value="Shop">
+      </div>
+    </form>
+
 </body>
 </html>
